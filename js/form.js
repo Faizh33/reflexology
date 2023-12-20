61f172ac13d528f03d2b725bd3a64f5865a0d58e
Erika REDON <erika_redon@hotmail.com>
2023-12-20 17:41:18 +0100

    creating a script with ajax

---
+++ js/form.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    // Intercepter la soumission du formulaire
+    $("#contactForm").submit(function(event) {
+        event.preventDefault();
+
+        // Sérialiser les données du formulaire
+        var formData = $(this).serialize();
+
+        // Envoyer les données du formulaire via Ajax
+        $.ajax({
+            type: "POST",
+            url: "contact.php",
+            data: formData,
+            dataType: 'json', 
+            success: function(response) {
+                if (response.success !== null) {
+                    // Afficher la div de succès ou d'erreur en fonction de la réponse
+                    if (response.success) {
+                        $("#successContainer").css("display", "inline-block");
+                    } else {
+                        $("#errorContainer").html("Erreur lors de l'envoi du message.");
+                        $("#errorContainer").css("display", "inline-block");
+                    }
+                } else {
+                    console.error("Réponse du serveur invalide.");
+                }
+            },            
+            error: function(xhr, textStatus, errorThrown) {
+                console.error("Erreur Ajax :", errorThrown);
+            }
+        });
+    });
+});
